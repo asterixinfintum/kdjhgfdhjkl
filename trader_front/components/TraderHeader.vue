139c1914acda28846f8d7019aa81ab1573040e3b
Your Name <you@example.com>
2025-09-15 20:24:28 -0700

    i

--- trader_front/components/TraderHeader.vue
+++ trader_front/components/TraderHeader.vue
@@ -8,7 +8,7 @@
       <div class="trader__header--area">
         <div class="trader__header--logo" v-if="client">
           <figure @click="$router.push('/overview')">
-            <img :src="`${primaryapi}/traderheaderlogotransparent.png`" />
+            <img src="/XRP_LOGO.0257a8ba.png" />
           </figure>
 
           <div class="mobile-menu-logo" @click="openmobilemenu">
@@ -20,7 +20,7 @@
 
         <div class="trader__header--logo" v-if="!client" @click="$router.push('/')">
           <figure>
-            <img :src="`${primaryapi}/traderheaderlogotransparent.png`" />
+            <img src="/XRP_LOGO.0257a8ba.png" />
           </figure>
         </div>
 
